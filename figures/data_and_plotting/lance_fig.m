224efa61a99e3dc28ed0ce4a627f86d39c619dc7
alexlukin <alexandre.lukin@gmail.com>
2018-09-07 15:02:57 -0400

    figure edits

--- figures/data_and_plotting/lance_fig.m
+++ figures/data_and_plotting/lance_fig.m
@@ -4,12 +4,14 @@ plot(sinc([-1.99:0.01:2].*pi).^2)
 % plot(((cos(0.5*pi^2*[-1.99:0.01:2]))).^2)
 % hold off
 hold on
-plot(((1+cos(pi^2*[-1.99:0.01:2]))/2))
+plot(((1+cos(pi^2*[-1.99:0.01:2]))/2).^2)
 hold off
 box off
 % xticks off
 
-xx = [-1.99:0.01:2];
-psf = @(x) sinc(pi.*x)
+aa=10;
+xx = [-10:0.01:10];
 figure(141)
-plot(xx,psf(xx).^2)
+plot(xx,sinc(aa.*(xx-1/(2*aa)))+sinc(aa.*(xx+1/(2*aa)))+sinc(aa.*(xx-1/(2*aa)-10/aa))+sinc(aa.*(xx+1/(2*aa)-10/aa)))
+
+
